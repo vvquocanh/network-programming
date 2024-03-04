63fccc9f166d0354bb05339d930f86f1e2e54434
Vũ Vương Quốc Anh <quanhuk0903@gmail.com>
2024-03-04 14:41:04 +0100

    Update lab 2 exercise 1

--- Lab 2/Exercise 1/server.c
+++ Lab 2/Exercise 1/server.c
@@ -5,105 +5,189 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+#define SERVERPORT 8888
 #define MYMSGLEN 2048
+#define MAXQUEUE 3
 
-int palindrome ( char * s )
+int palindrome (char *s)
 {
-	char * t = s + strlen ( s ) - 1 ;
-
-	while ( * s == * t )
-		s ++, t -- ;
-	return s >= t ;
+	char *t = s + strlen(s) - 1;
+	
+	while (*s == *t)
+		s++, t--;
+		
+	return s >= t;
 }
 
-int main(int argc, char *argv[]) {
+int create_socket() {
+	int sock;
 	
-	int socket_desc, client_sock;
-	int socket_size, read_size;
-	struct sockaddr_in server, client;
-	char client_message[MYMSGLEN];
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 	
-	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-	
-	if (socket_desc == -1) {
-		printf("Could not create socket\n");
-		return -1;
-	}
-	
-	printf("socket created\n");
-	
-	int code, enable = 1;
-	code = setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
-	if (code == -1) {
-		perror("setsockopt");
-		return 1;
-	}
-	
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_family = AF_INET;
-	server.sin_port = htons(8888);
-	
-	if (bind(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0) {
-		//print the error message
-		perror("bind failed. Error");
-		close(socket_desc);
-		return -1;
-	}
-	
-	printf("bind done\n");
-	
-	code = listen(socket_desc, 3);
-	
-	if (code == -1) {
-		perror("listen");
+	if (sock == -1) {
+		perror("Couldn't create socket\n");
 		exit(1);
 	}
 	
-	printf("Connection accepted\n");
+	printf("Socket created\n");
+	
+	return sock;
+}
+
+void set_socket_option(int sock) {
+	int enable = 1;
+	
+	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) == -1) {
+		perror("Fail to set socket option\n");
+		exit(1);
+	}
+	
+	
+	if (setsockopt(sock, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int)) == -1) {
+		perror("Fail to set socket option\n");
+		exit(1);
+	}
+}
+
+void setup_server(struct sockaddr_in *addr) {
+	memset(addr, 0, sizeof(struct sockaddr_in));
+	
+	addr->sin_addr.s_addr = INADDR_ANY;
+	addr->sin_family = AF_INET;
+	addr->sin_port = htons(SERVERPORT);	
+}
+
+void bind_socket(int sock, struct sockaddr_in *addr) {
+	if (bind(sock, (struct sockaddr*) addr, sizeof(struct sockaddr_in)) == 0) {
+		printf("Bind done\n");
+		return;
+	};
+	
+	perror("Fail to bind socket\n");
+	close(sock);
+	exit(1);
+}
+
+void listen_to_client(int sock) {
+	if (listen(sock, MAXQUEUE) != -1) {
+		printf("Connection accepted\n");
+		return;
+	}
+	
+	perror("Fail to listen to client");
+	close(sock);
+	exit(1);
+}
+
+int start_server() {
+	int sock = create_socket();
+	
+	set_socket_option(sock);
+	
+	struct sockaddr_in server;
+	setup_server(&server);
+	
+	bind_socket(sock, &server);
+	
+	listen_to_client(sock);
+	
+	return sock;
+}
+
+int accept_connection(int server_sock, struct sockaddr_in *client) {
+	socklen_t len = sizeof(struct sockaddr_in);
+	int client_sock = accept(server_sock, (struct sockaddr *) client, &len);
+	if (client_sock != -1) return client_sock;
+	
+	perror("Fail to accept client");
+	return -1;
+}
+
+void print_socket_information(int sock, struct sockaddr_in *addr) {
+	socklen_t len = sizeof(struct sockaddr_in);
+	
+	int code = getsockname(sock, (struct sockaddr*) addr, &len);
+	if (code == -1) {
+		perror("Fail to get local socket information");
+		return;
+	}
+	printf("The local address bound to the current socket --> %s:%d \n", 
+		inet_ntoa(addr->sin_addr), ntohs(addr->sin_port));
+		
+	code = getpeername(sock, (struct sockaddr*) addr, &len);
+	if (code == -1) {
+		perror("Fail to get peer socket information");
+		return;
+	}
+	printf("The peer address bound to the peer socket --> %s:%d \n", 
+		inet_ntoa(addr->sin_addr), ntohs(addr->sin_port));
+}
+
+int establish_connection(int server_sock) {
+	printf("\nWaiting for incoming connections ....\n");
+	
+	struct sockaddr_in client;
+	int client_sock = accept_connection(server_sock, &client);
+	
+	print_socket_information(client_sock, &client);
+	
+	return client_sock;
+}
+
+ssize_t receive_message(int client_sock, char client_message[]) {
+	printf("Waiting for a string to process....\n" ) ;
+	
+	memset(client_message, 0, MYMSGLEN);
+	 
+	return recv(client_sock, client_message, MYMSGLEN, 0);
+}
+
+int answer_message(int client_sock, char client_message[]) {
+	int result = palindrome(client_message);
+	
+	return send(client_sock, &result, sizeof(int), 0);
+}
+
+void response(int client_sock) {
+	char client_message[MYMSGLEN];
+	
+	while(1)
+	{
+	  	ssize_t read_size = receive_message(client_sock, client_message);
+	  	
+		if (read_size == 0) {
+			printf("Client disconnected\n");
+			fflush(stdout);
+			break;
+		}
+		else if (read_size == -1) {
+			perror("Failed to receive from client\n");
+			break;
+		}
+		
+		printf("Client message: %s\n", client_message);
+		
+		if (answer_message(client_sock, client_message) != -1) continue;
+		
+		perror("Fail to answer client");
+		break;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	int server_sock = start_server();
 	
 	while (1) {
-		printf("\nWaiting for incoming connections ....\n");
-	
-		socket_size = sizeof(struct sockaddr_in);
-	
-		client_sock = accept(socket_desc, (struct sockaddr *) &client, (socklen_t *) &socket_size);
 		
-		if (getpeername(client_sock, (struct sockaddr *) &client, (socklen_t *) &socket_size) != 0) {
-			perror("Failed to get user's socket name");
+		int client_sock = establish_connection(server_sock);
+		
+		if (client_sock == -1) {
+			perror("Fail to accept connection from clint");
 			continue;
 		}
 		
-		printf("A new client is connected with:\n\t"
-		"- Client address: %s\n\t"
-		"- Client port: %d\n",
-		inet_ntoa(client.sin_addr), ntohs(client.sin_port));
-		
-		if (client_sock < 0) {
-			close(socket_desc);
-			perror("accept failed");
-			continue;
-		}
-		
-		while ((read_size = recv(client_sock, client_message, MYMSGLEN, 0)) > 0) {
-		
-			client_message[read_size] = '\0';
-			printf("Msg received: %s, size of the message received, %d\n", client_message, read_size);
-			
-			char* reply_message = palindrome(client_message) ? "The string is palindrome" : "The string is NOT palindrome";
-			
-			write(client_sock, reply_message, strlen(reply_message));
-		} 
-		
-		if (read_size == 0) {
-			printf("client disconnected\n");
-			fflush(stdout);
-		}
-		
-		else if (read_size == -1) {
-			printf("recv failed\n");
-		}
+		response(client_sock);
 	}
-
 	
 	return 0;
 }
