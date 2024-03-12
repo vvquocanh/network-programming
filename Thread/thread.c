503a70949dda7d2573886e750caec5abc703e644
Vũ Vương Quốc Anh <quanhuk0903@gmail.com>
2024-03-12 19:31:23 +0100

    Finish thread

--- Thread/thread.c
+++ Thread/thread.c
@@ -6,15 +6,15 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define THREADNUMBER 2
+#define THREADNUMBER 10
 
-void *thread_routine() {
+void *thread_routine(void *rank) {
+	sleep(1);
+	
 	pid_t tid = gettid();
 	
-	printf("\nProcess %d, thread: %ld: Hello World!\n", getpid(), (long)tid);
-	
-	sleep(5);
-	
+	printf("\nProcess %d, thread: %ld, rank: %d: Hello World!\n", getpid(), (long)tid, *((int *)rank));
+		
 	printf("\nThread exit!\n");
 	
 	pthread_exit(NULL);
@@ -24,16 +24,20 @@ int main ()
 {
  	pthread_t tid[THREADNUMBER];
  	
- 	for (int i = 1; i <= 2; i++) {
- 		int result = pthread_create(&(tid[i]), NULL, &thread_routine, NULL);
+ 	for (int i = 0; i < THREADNUMBER; i++) {
+ 		
+ 		int *new_value = malloc(sizeof(*new_value));
+ 		*new_value = i;
+ 		
+ 		int result = pthread_create(&(tid[i]), NULL, &thread_routine, new_value);
  		
  		if (result == 0) printf("\nThread %d created ...\n", i);
  		else printf("Cannot create thread: %s \n", strerror(result));
- 		
- 		sleep(2);
  	}
  	
- 	sleep(10);
+ 	for (int i = 0; i < THREADNUMBER; i++) {
+ 		pthread_join(tid[i], NULL);
+ 	}
  	
  	printf("Main program exit\n");
  	
